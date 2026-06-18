ca42a1247176b03ee43c409b9f2dfe66199a607b
tasneem youssef <tasneemyoussef61@gmail.com>
2026-06-18 23:43:15 +0300

    feat: initialize main App component with React Router and defined route structure

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,40 @@ const App = () => {
         <BackToTop />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/trips" element={<Trips />} />
-          <Route path="/hotels" element={<Hotels />} />
-          <Route path="/activities" element={<Activities />} />
+          <Route path="/trips" element={<Trips />} >
+            <Route path="trip1" />
+            <Route path="trip2" />
+            <Route path="trip3" />
+            <Route path="trip4" />
+            <Route path="trip5" />
+            <Route path="trip6" />
+          </Route>
+          <Route path="/hotels" element={<Hotels />} >
+            <Route path="hotel1" />
+            <Route path="hotel2" />
+            <Route path="hotel3" />
+            <Route path="hotel4" />
+            <Route path="hotel5" />
+            <Route path="hotel6" />
+          </Route>
+          <Route path="/activities" element={<Activities />} >
+            <Route path="activity1" />
+            <Route path="activity2" />
+            <Route path="activity3" />
+            <Route path="activity4" />
+            <Route path="activity5" />
+            <Route path="activity6" />
+          </Route>
           <Route path="/login" />
           <Route path="/signup" />
-          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/destinations" element={<Destinations />} >
+            <Route path="destination1" />
+            <Route path="destination2" />
+            <Route path="destination3" />
+            <Route path="destination4" />
+            <Route path="destination5" />
+            <Route path="destination6" />
+          </Route>
           <Route path="/contact" />
           <Route path="*" element={<NotFound />} />
         </Routes>
